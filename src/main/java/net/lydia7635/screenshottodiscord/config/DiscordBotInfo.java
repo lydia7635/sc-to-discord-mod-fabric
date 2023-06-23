9892676bc724f18fec977ba24aa788a37c5af8ec
lydia7635 <lydia7635@gmail.com>
2023-06-23 17:49:14 +0800

    temp: test minecraft 1.20

---
+++ src/main/java/net/lydia7635/screenshottodiscord/config/DiscordBotInfo.java
@@ -0,0 +1,6 @@
+package net.lydia7635.screenshottodiscord.config;
+
+public class DiscordBotInfo {
+    public static final String botToken = "YOUR-BOT-TOKEN";
+    public static final String channelId = "DISCORD-CHANNEL-ID";
+}
